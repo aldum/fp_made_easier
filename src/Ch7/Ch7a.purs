cfe1adbc7ca548f23cb26263d7b18abe56fbe353
aldum <aldum@artixlinux.org>
2022-12-24 12:38:54 +0100

    7.2

--- src/Ch7/Ch7a.purs
+++ src/Ch7/Ch7a.purs
@@ -1,2 +1,11 @@
 module Ch7.Ch7a where
 
+
+-- data Maybe a = Just a | Nothing
+data Option a = Some a | None -- let's Scala this up a bit
+
+someInt :: Option Int
+someInt = Some 1
+
+none :: ∀ a. Option a
+none = None
--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,10 @@ module Main where
 
 import Prelude
 
-import Ch6.Multiple (t, tu)
 import Effect (Effect)
 import Effect.Console (log)
--- import Undefined (undefined)
+import Undefined (undefined)
+
 
 main :: Effect Unit
-main = do
-  log $ show $ t
-  log $ show $ tu
+main = undefined
