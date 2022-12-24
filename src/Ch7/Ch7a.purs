4ff214f3e766b5e69199fca6a3c4f42f4aec9043
aldum <aldum@artixlinux.org>
2022-12-24 15:21:20 +0100

    7.14 derive Eq, Ord

--- src/Ch7/Ch7a.purs
+++ src/Ch7/Ch7a.purs
@@ -23,24 +23,26 @@ test = do
   log $ show $ (None :: Option Unit) -- ❸ Prints None.
 
 -- 7.12
-instance optionShow :: Show a => Show (Option a) where
-  show (Some s) = "Some(" <> show s <> ")"
-  show None     = "None"
+-- instance optionShow :: Show a => Show (Option a) where
+--   show (Some s) = "Some(" <> show s <> ")"
+--   show None     = "None"
+--  derive instance optionShow :: Show a => Show (Option a)
+derive instance optionShow :: Show (Option a)
 
 -- 7.5
--- derive instance optionEq :: Eq a => Eq (Option a)
-instance optionEq :: Eq a => Eq (Option a) where
-  eq (Some a1) (Some a2) = a1 == a2
-  eq None      None      = true
-  eq _         _         = false
+derive instance optionEq :: Eq a => Eq (Option a)
+-- instance optionEq :: Eq a => Eq (Option a) where
+--   eq (Some a1) (Some a2) = a1 == a2
+--   eq None      None      = true
+--   eq _         _         = false
 
--- derive instance optionOrd :: Ord a => Ord (Option a)
-instance optionOrd :: Ord a => Ord (Option a) where
-  -- compare o1 o2 = undefined
-  compare None     None     = EQ
-  compare None     (Some _) = LT
-  compare (Some _) None     = GT
-  compare (Some a) (Some b) = a `compare` b
+derive instance optionOrd :: Ord a => Ord (Option a)
+-- instance optionOrd :: Ord a => Ord (Option a) where
+--   -- compare o1 o2 = undefined
+--   compare None     None     = EQ
+--   compare None     (Some _) = LT
+--   compare (Some _) None     = GT
+--   compare (Some a) (Some b) = a `compare` b
 
 
 greaterThanOrEq :: ∀ a. Ord a => a -> a -> Boolean
